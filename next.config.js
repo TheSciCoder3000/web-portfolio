7b70ccf30a13362345572881d020aaa55335e52d
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2023-03-28 23:46:40 +0800

    sass compatibility update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
