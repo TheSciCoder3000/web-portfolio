c95b1cd8407f92466b76333e2c3e9adcc0c3a798
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2023-03-30 22:50:17 +0800

    hero and about me section update

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,24 +1,48 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const { pathname } = useRouter();
+  const navLinks = [
+    { name: "Home", path: "/" },
+    { name: "About", path: "/about" },
+    { name: "Skills", path: "/skills" },
+    { name: "Projects", path: "/projects" },
+  ];
+
+  console.log(pathname);
+
   return (
     <div className="home-cont">
       {/* Navigation */}
       <div className="nav-cont">
-        <Link href="">Home</Link>
-        <Link href="/about" passHref>
-          About
-        </Link>
-        <Link href="/skills" passHref>
-          Skills
-        </Link>
-        <Link href="/projects" passHref>
-          Projects
-        </Link>
+        {navLinks.map((nav, indx) => (
+          <Link
+            key={indx}
+            className={`nav-link ${pathname == nav.path ? "disabled" : ""}`}
+            href={nav.path}
+          >
+            {nav.name}
+          </Link>
+        ))}
       </div>
 
       {/* Hero Section */}
-      <div className="hero-cont"></div>
+      <div className="hero-cont">
+        <h1 className="hero-title">Hi, I'm John Juvi</h1>
+        <h1 className="hero-title">A PROGRAMMER</h1>
+      </div>
+
+      {/* About Me Section */}
+      <div className="about-cont">
+        <h2 className="section-header">About Me</h2>
+        <p className="about-content">
+          Hello my name is John Juvi De Villa and I’m from the Philippines. I
+          have experiences in programming such as game development (Godot) and
+          app development but I am most experienced in the field of web
+          development using Reactjs library.
+        </p>
+      </div>
     </div>
   );
 }
