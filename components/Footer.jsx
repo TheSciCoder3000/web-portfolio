60f9f2e346396b6d30aacce7251b02160cf5f562
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2023-04-01 22:03:46 +0800

    footer feature add

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <div className="footer-cont">
+      <div className="footer-content">
+        <div className="footer-col footer-media"></div>
+        <div className="footer-col footer-links"></div>
+      </div>
+      <p className="copyright">@John Juvi De Villa. All rights reserved</p>
+    </div>
+  );
+}
+
+export default Footer;
