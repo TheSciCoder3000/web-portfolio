271d136e9eb0df96db7b69a26215cf8c12f7c6d0
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2023-04-08 22:10:14 +0800

    project detail page update

--- pages/projects/[projectId].jsx
+++ pages/projects/[projectId].jsx
@@ -1,9 +1,16 @@
 import { useRouter } from "next/router";
+import Navbar from "@components/Navbar";
+import { navLinks } from "@utils/content";
 
 const ProjectDetails = () => {
   const router = useRouter();
   const projectId = router.query.projectId;
-  return <div>Project Details: {projectId}</div>;
+
+  return (
+    <div className="project-details-cont">
+      <Navbar navLinks={navLinks} />
+    </div>
+  );
 };
 
 export default ProjectDetails;
