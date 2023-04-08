d47a53c84368c3e3cb4b834128399411be27c747
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2023-04-08 22:10:42 +0800

    isolating some animation variants and content vars

---
+++ utils/content.jsx
@@ -0,0 +1,38 @@
+import StoreImg from "@styles/img/Store_App_img.jpg";
+import LandingImg from "@styles/img/yelan.jpg";
+import KalikasanImg from "@styles/img/kalikasan_img.jpg";
+import SchoolImg from "@styles/img/ningguang.jpg";
+
+export const projectCollection = [
+  {
+    id: 0,
+    name: "Store App",
+    desc: "An online shop website built during the pandemic in an effort to information and data regarding our small-time business accessible to our subdivision.",
+    img: StoreImg,
+  },
+  {
+    id: 1,
+    name: "Landing page",
+    desc: "Lorem ipsum dolors sit amet, consectetur adipiscing elit. Sed at risus quis enim venenatis maximus nec eget tortor. Sed sed dui sit amet risus semper luctus. Aliquam dictum elementum imperdiet. Fusce vel metus ut leo commodo pulvinar nec ut leo. Nullam iaculis, arcu ut bibendum fringilla, libero metus molestie lectus, ac malesuada mauris turpis vel eros. Quisque interdum enim risus. Mauris dignissim vel ipsum vitae hendrerit. Aliquam nec vulputate magna. Praesent ullamcorper tempus dolor suscipit pretium. Etiam hendrerit lacinia ex, id euismod dolor congue at. Etiam accumsan elit et justo semper, sed semper tortor rhoncus. Nam sagittis sodales risus sit. ",
+    img: LandingImg,
+  },
+  {
+    id: 2,
+    name: "Kalikasan",
+    desc: "Website blog application built for our NSTP Advocacy Campaign project during our 1st year in college. Project contains a landing page for visitors and a dedictaed web application to host activites and upload lessons and modules.",
+    img: KalikasanImg,
+  },
+  {
+    id: 3,
+    name: "Online School Tools",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at risus quis enim venenatis maximus nec eget tortor. Sed sed dui sit amet risus semper luctus. Aliquam dictum elementum imperdiet. Fusce vel metus ut leo commodo pulvinar nec ut leo. Nullam iaculis, arcu ut bibendum fringilla, libero metus molestie lectus, ac malesuada mauris turpis vel eros. Quisque interdum enim risus. Mauris dignissim vel ipsum vitae hendrerit. Aliquam nec vulputate magna. Praesent ullamcorper tempus dolor suscipit pretium. Etiam hendrerit lacinia ex, id euismod dolor congue at. Etiam accumsan elit et justo semper, sed semper tortor rhoncus. Nam sagittis sodales risus sit. ",
+    img: SchoolImg,
+  },
+];
+
+export const navLinks = [
+  { name: "Home", path: "/" },
+  { name: "About", path: "/about" },
+  { name: "Skills", path: "/skills" },
+  { name: "Projects", path: "/projects" },
+];
