11a1920c769ec0535d5a84e46196b205e7688613
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2023-04-21 22:16:08 +0800

    about page structure feature update

--- pages/about.jsx
+++ pages/about.jsx
@@ -14,8 +14,46 @@ const about = () => {
         <h1 className="page-title">About Me</h1>
       </div>
 
+      {/* Value Proposition */}
+      <div className="value-proposition">
+        <h1 className="value-text">
+          “Every website should be reliable with good quality, effectively
+          delivering the customer’s goals and vision”
+        </h1>
+      </div>
+
       {/* About Content Section */}
-      <div className="content-section"></div>
+      <div className="content-section">
+        <div className="section-cont">
+          <div className="img-cont img-1"></div>
+          <div className="text-cont">
+            <h2 className="user-name">John Juvi Q.</h2>
+            <h2 className="user-lastname">De Villa</h2>
+            <p className="about-text-content">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget
+              nibh non ante blandit ullamcorper ut in ex. Vivamus nec pulvinar
+              nunc. Suspendisse at pharetra nunc, nec dapibus nunc. Nulla cursus
+              justo sit amet posuere venenatis. In hac habitasse platea
+              dictumst. Pellentesque ultrices turpis sem, sed efficitur sem
+              ultrices id. In feugiat elementum massa convallis fringilla.
+              Aliquam ac vulputate ante.
+            </p>
+          </div>
+        </div>
+        <div className="section-cont">
+          <div className="text-cont">
+            <p className="about-text-content">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget
+              nibh non ante blandit ullamcorper ut in ex. Vivamus nec pulvinar
+              nunc. Suspendisse at pharetra nunc, nec dapibus nunc. Nulla cursus
+              justo sit amet posuere venenatis. In hac habitasse platea
+              dictumst. Pellentesque ultrices turpis sem, sed efficitur sem
+              ultrices id.
+            </p>
+          </div>
+          <div className="img-cont img-2"></div>
+        </div>
+      </div>
 
       {/* Footer */}
       <Footer navLinks={navLinks} />
