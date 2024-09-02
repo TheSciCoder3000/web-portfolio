dacaa7d917de10c52c08bb4377ec11cd045c9e4e
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2024-09-02 22:41:19 +0800

    menu bar navbar update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,36 +1,49 @@
 import { useRouter } from "next/router";
 import LogoSvg from "@svg/LogoSvg";
+import MenuSvg from "@svg/MenuSvg";
 import ScrollLink from "@utils/ScrollLink";
 import Link from "next/link";
+import { useWindowDimensions } from "@utils/hooks";
 
 function Navbar({ navLinks }) {
   const { pathname } = useRouter();
+  const { width } = useWindowDimensions();
+
   return (
     <div className="nav-cont">
       <LogoSvg className="nav-logo" />
-      {navLinks.map((nav, indx) => {
-        if ((pathname == "/" && nav.name == "Home") || pathname != "/") {
-          return (
-            <Link
-              key={indx}
-              className={`nav-link ${pathname == nav.path ? "disabled" : ""}`}
-              href={nav.path}
-            >
-              {nav.name}
-            </Link>
-          )
-        } else {
-          return (
-            <ScrollLink
-              key={indx}
-              className={`nav-link ${pathname == nav.path ? "disabled" : ""}`}
-              href={nav.scroll}
-            >
-              {nav.name}
-            </ScrollLink>
-          )
-        }
-      })}
+
+      {width > 1000 ? 
+      <>
+        {navLinks.map((nav, indx) => {
+          if ((pathname == "/" && nav.name == "Home") || pathname != "/") {
+            return (
+              <Link
+                key={indx}
+                className={`nav-link ${pathname == nav.path ? "disabled" : ""}`}
+                href={nav.path}
+              >
+                {nav.name}
+              </Link>
+            )
+          } else {
+            return (
+              <ScrollLink
+                key={indx}
+                className={`nav-link ${pathname == nav.path ? "disabled" : ""}`}
+                href={nav.scroll}
+              >
+                {nav.name}
+              </ScrollLink>
+            )
+          }
+        })}
+      </> 
+      : 
+      <>
+        <MenuSvg  className="nav-menu" />
+      </>}
+      
     </div>
   );
 }
