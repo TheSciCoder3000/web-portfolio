b96b1e1ef231cf73454e4a36bf8a0d3ff86ce7c4
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-08-03 19:14:07 +0800

    updated next libraray

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import styles from "./page.module.scss";
+
+export default function Home() {
+  return (
+    <div>
+      {/* Hero Section */}
+      <div>
+        <h1 className={styles.h1}>JOHN JUVI Q. DE VILLA</h1>
+      </div>
+    </div>
+  );
+}
