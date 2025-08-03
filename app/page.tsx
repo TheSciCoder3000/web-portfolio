3191cf16f5fbd6d1f889d1d3d19854e59f080d70
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-08-03 21:19:25 +0800

    hero and about sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,44 @@
-import styles from "./page.module.scss";
+import styles from "./styles/global.module.scss";
+import heroStyles from "./styles/hero.module.scss";
+import aboutStyles from "./styles/about.module.scss";
 
 export default function Home() {
   return (
-    <div>
+    <div className={heroStyles["home-page"]}>
       {/* Hero Section */}
-      <div>
-        <h1 className={styles.h1}>JOHN JUVI Q. DE VILLA</h1>
+      <div className={heroStyles.heroSection}>
+        {/* Hero Name */}
+        <div className={heroStyles.heroName}>
+          <h1 className={styles.h1}>JOHN JUVI DE VILLA</h1>
+          <hr className={heroStyles.headerBar} />
+        </div>
+
+        <img src="/hero-portrait.png" className={heroStyles.heroImg} />
+
+        <div className={heroStyles["hero-box"]}></div>
+      </div>
+
+      {/* About Section */}
+      <div className={aboutStyles.aboutSection}>
+        <div className={aboutStyles.aboutBox}>
+          <div className={aboutStyles.aboutContent}>
+            <div className={aboutStyles.aboutHeader}>
+              <h3 className={styles.h3}>ABOUT ME</h3>
+              <h2 className={styles.h2}>Fresh and New</h2>
+            </div>
+            <p className={styles.p}>
+              i am a Computer Engineer graduate specializing in Software
+              Development. Experienced in frontend web development using React
+              and Typescript with solid foundation in backend development using
+              Nodejs and Django.
+            </p>
+            <a className={`${styles.a} ${aboutStyles.aboutLink}`}>
+              Download My Resume
+            </a>
+          </div>
+
+          <img className={aboutStyles.aboutImg} src="/grad.jpg" />
+        </div>
       </div>
     </div>
   );
