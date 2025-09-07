78d7b1a0f0134076a4d1069b4870edfe4d5bca4b
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-09-08 00:31:09 +0800

    added new project entry

--- app/data.ts
+++ app/data.ts
@@ -9,9 +9,35 @@ export interface IProject {
 
 export const projects: IProject[] = [
   {
+    title: "SoundWave | Headset Shop",
+    description: `
+      Developed a full-stack web application leveraging ReactJS 
+      and NextJS for both the frontend and backend. Implemented 
+      Supabase as a comprehensive backend solution for database 
+      management, file storage, and user authentication. Integrated 
+      Stripe as the payment gateway to enable secure and seamless 
+      transactions. Managed global application state efficiently using 
+      Redux Toolkit, ensuring scalability and maintainability of the application.
+    `,
+    ss: [
+      "/soundwave/home.png",
+      "/soundwave/products.png",
+      "/soundwave/product-detail.png",
+      "/soundwave/cart.png",
+    ],
+    github: "https://github.com/TheSciCoder3000/ear-commerce",
+    live: "https://soundwave.neurocoder.work",
+    bkg: "red",
+  },
+  {
     title: "Kalikasan Advocacy",
-    description:
-      "The kalikasan Advocacy is a website designed to promote awareness of global warming  and climate change. Built with a responsive landing page and a LMS web app. Users can engage in activities to promote awareness in the community. It is equipped with an admin account that allows management of users and submissions.",
+    description: `
+      The kalikasan Advocacy is a website designed to promote awareness 
+      of global warming  and climate change. Built with a responsive 
+      landing page and a LMS web app. Users can engage in activities to 
+      promote awareness in the community. It is equipped with an admin 
+      account that allows management of users and submissions.
+    `,
     ss: ["/kalikasan/1.webp", "/kalikasan/2.webp", "/kalikasan/3.webp"],
     github: "https://github.com/TheSciCoder3000/kalikasan-blog",
     live: "https://kalikasan-blog.firebaseapp.com",
@@ -19,8 +45,12 @@ export const projects: IProject[] = [
   },
   {
     title: "CID Architecture",
-    description:
-      "A basic demo landing page of a construction and architectural company. It is built with responsive layout and fluid animations, keeping users hooked. It is built with multiple pages detailing what the company is about and the projects they have organized. ",
+    description: `
+      A basic demo landing page of a construction and architectural company. 
+      It is built with responsive layout and fluid animations, keeping users 
+      hooked. It is built with multiple pages detailing what the company is 
+      about and the projects they have organized. 
+    `,
     ss: ["/archi/1.webp", "/archi/2.webp", "/archi/3.webp"],
     github: "https://github.com/TheSciCoder3000/archi_vite",
     live: "https://cid-archi.web.app/",
@@ -28,8 +58,12 @@ export const projects: IProject[] = [
   },
   {
     title: "The Villa Store",
-    description:
-      "One of the first websites I created during the Pandemic. The web app was inspired to help members of the community become updated if our residence was selling any goods they were interested. It built using generic designs from bootstrap and and the Django was used for the backend.",
+    description: `
+      One of the first websites I created during the Pandemic. The web app 
+      was inspired to help members of the community become updated if our 
+      residence was selling any goods they were interested. It built using 
+      generic designs from bootstrap and and the Django was used for the backend.
+    `,
     ss: ["/store/1.webp", "/store/2.webp", "/store/3.webp", "/store/4.webp"],
     github: "https://github.com/TheSciCoder3000/Store-App",
     live: "https://thescicoder3000.pythonanywhere.com",
