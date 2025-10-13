f0d47917b70241cc4199053159c1ab043b9c98d4
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-10-13 23:50:43 +0800

    /src project structure

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,16 @@
+import About from "@//components/About";
+import Contact from "@//components/Contact";
+import HeroSection from "@//components/Hero";
+import Project from "@//components/Projects";
+
+export default function Home() {
+  return (
+    // <div className="w-full dark:bg-[#24292E]">
+    <div className="home-page w-full transition-colors duration-500 dark:bg-gray-900">
+      <HeroSection />
+      <About />
+      <Project />
+      <Contact />
+    </div>
+  );
+}
---
+++ src/components/Payload/Header/Component.tsx
@@ -0,0 +1,11 @@
+import { HeaderClient } from "./Component.client";
+import { getCachedGlobal } from "@//lib/payload/utilities/getGlobals";
+import React from "react";
+
+import type { Header } from "@//lib/payload/payload-types";
+
+export async function Header() {
+  const headerData: Header = await getCachedGlobal("header", 1)();
+
+  return <HeaderClient data={headerData} />;
+}
