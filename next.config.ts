ae234aa5110b0e9ff21ccdec7984f29e51fe5b12
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2025-10-14 15:01:28 +0800

    adding dynamic host patterns

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,22 @@
 import { withPayload } from "@payloadcms/next/withPayload";
 import redirects from "./redirect.js";
 import type { NextConfig } from "next";
+import { RemotePattern } from "next/dist/shared/lib/image-config.js";
 
 const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
   : process.env.__NEXT_PRIVATE_ORIGIN || "http://localhost:3000";
+
+const patterns = [NEXT_PUBLIC_SERVER_URL /* 'https://example.com' */].map(
+  (item) => {
+    const url = new URL(item);
+
+    return {
+      hostname: url.hostname,
+      protocol: url.protocol.replace(":", ""),
+    } as RemotePattern;
+  },
+);
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -15,6 +27,7 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "cdn.simpleicons.org",
       },
+      ...patterns,
     ],
   },
   webpack: (webpackConfig) => {
